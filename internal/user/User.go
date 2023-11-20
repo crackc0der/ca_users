a610808babf507f66bda9feea47ab8b15b89b608
crackc0der <natsaev1989@gmail.com>
2023-11-20 19:54:08 +0300

    second commit

--- internal/user/User.go
+++ internal/user/User.go
@@ -1,1 +1,10 @@
 package user
+
+type User struct {
+	UserID       int    `json:"userId"`
+	Fname        string `json:"fname"`
+	Lname        string `json:"lname"`
+	Age          int    `json:"age"`
+	Email        string `json:"email"`
+	PasswordHash []byte `json:"passwordHash"`
+}
