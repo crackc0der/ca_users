0ee513a7e1637d72e53b24c7147ddbb499a30e54
crackc0der <natsaev1989@gmail.com>
2023-11-25 22:39:56 +0300

    add cmd folder and service interface

--- internal/user/Endpoint.go
+++ internal/user/Endpoint.go
@@ -1,1 +1,11 @@
 package user
+
+import "context"
+
+type service interface {
+	AddUser(ctx context.Context, user *User) (*User, error)
+	UpdateUser(ctx context.Context, user *User) (*User, error)
+	DeleteUser(ctx context.Context, userID int) error
+	GetAllUsers(ctx context.Context) ([]User, error)
+	GetUser(ctx context.Context, userID int) (*User, error)
+}
