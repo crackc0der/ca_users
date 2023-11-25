30d5d4a9c0bb32125a67f6d88099d773fb12c1e4
crackc0der <natsaev1989@gmail.com>
2023-11-25 23:21:14 +0300

    new commit

--- internal/user/Endpoint.go
+++ internal/user/Endpoint.go
@@ -1,6 +1,10 @@
 package user
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
 type service interface {
 	AddUser(ctx context.Context, user *User) (*User, error)
@@ -8,4 +12,42 @@ type service interface {
 	DeleteUser(ctx context.Context, userID int) error
 	GetAllUsers(ctx context.Context) ([]User, error)
 	GetUser(ctx context.Context, userID int) (*User, error)
+}
+
+func NewEndpoint(service *Service) *Endpoint {
+	return &Endpoint{service: *service}
+}
+
+type Endpoint struct {
+	service Service
+}
+
+func (e *Endpoint) GetUser(writer http.ResponseWriter, request *http.Request) {
+	var userID int
+	if err := json.NewDecoder(request.Body).Decode(&userID); err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+
+	user, err := e.service.GetUser(request.Context(), 45)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+
+	json.NewEncoder(writer).Encode(user)
+}
+
+func (e *Endpoint) AddUser(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (e *Endpoint) UpdateUser(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (e *Endpoint) DeleteUser(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (e *Endpoint) GetAllUsers(writer http.ResponseWriter, request *http.Request) {
+
 }
