000a54ec375812310ff029d5c88096fb7c6d7fba
crackc0der <natsaev1989@gmail.com>
2024-01-17 11:02:59 +0300

    fixed bug in uuid

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,14 +1,10 @@
 package user
 
-import (
-	"github.com/google/uuid"
-)
-
 type User struct {
-	UserID       uuid.UUID `json:"userId"`
-	Fname        string    `json:"fname"`
-	Lname        string    `json:"lname"`
-	Age          int       `json:"age"`
-	Email        string    `json:"email"`
-	PasswordHash string    `json:"passwordHash"`
+	UserID       int64  `json:"userId"`
+	Fname        string `json:"fname"`
+	Lname        string `json:"lname"`
+	Age          int    `json:"age"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"passwordHash"`
 }
